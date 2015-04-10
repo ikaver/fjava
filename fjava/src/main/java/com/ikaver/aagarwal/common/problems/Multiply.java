242bbd130ef20f853515aa4f58ff9f1a2876fa9f
Ankit Agarwal <ankit1990@gmail.com>
2015-04-10 18:52:31 -0400

    Adding karatsuba multiply benchmark for java fork join pool implementation.

---
+++ fjava/src/main/java/com/ikaver/aagarwal/common/problems/Multiply.java
@@ -0,0 +1,10 @@
+package com.ikaver.aagarwal.common.problems;
+
+import java.math.BigInteger;
+
+/** 
+ * Multiply two {@class BigInteger}.
+ */
+public interface Multiply {
+	public BigInteger multiply(BigInteger x, BigInteger y);
+}
