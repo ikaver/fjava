75e0e5fde0726135c4f91f2d9d9d9e2f349b5969
Iosef Kaver <iosef.kaver@gmail.com>
2015-04-17 20:12:31 -0400

    Renamed SID to RID

--- fjava/src/main/java/com/ikaver/aagarwal/fjava/ReceiverInitiatedDeque.java
+++ fjava/src/main/java/com/ikaver/aagarwal/fjava/ReceiverInitiatedDeque.java
@@ -5,31 +5,43 @@ import java.util.Deque;
 import java.util.Random;
 import java.util.concurrent.atomic.AtomicInteger;
 
-public class SenderInitiatedDeque<T> {
-  
+/**
+ * Represents a Sender Initiated Deque. This means that task runners that need
+ * tasks to run (receivers) are the ones who ask others for tasks.
+ */
+public class ReceiverInitiatedDeque {
+  //TODO: what is the run loop of this? Simple approach: Same thread as runner.
+
   public static final int VALID_STATUS = 1;
   public static final int INVALID_STATUS = 0;
   public static final int EMPTY_REQUEST = -1;
   
-  //TODO: make cache efficient? (False sharing) We don't need atomic here. 
+  //TODO: make cache efficient? (False sharing) 
+  //BTW, We don't need atomic here. 
   //Only need to make sure that writes are propagates to all threads.
   private AtomicInteger [] status; 
-  //TODO: make cache efficient? (False sharing)
-  private AtomicInteger [] requestCells;
-  //TODO: make cache efficient? (False sharing)
-  private T [] responseCells;
-  private T emptyTask;
   
-  private Deque<T> tasks;
+  //TODO: make cache efficient? (False sharing)
+  //requestCells[i] = j if task runner j is waiting for task runner i to give
+  //him work.
+  private AtomicInteger [] requestCells;
+  
+  //TODO: make cache efficient? (False sharing)
+  //responseCells[j] holds the task that task runner j stole from other task runner.
+  private FJavaTask [] responseCells;
+  
+  private FJavaTask emptyTask;
+  
+  private Deque<FJavaTask> tasks;
   
   private Random random;
 
   private int myIdx;
   private int numWorkers;
   
-  public SenderInitiatedDeque(AtomicInteger [] status, 
-      AtomicInteger [] requestCells, T [] responseCells, int myIdx, 
-      T emptyTask) {
+  public ReceiverInitiatedDeque(AtomicInteger [] status, 
+      AtomicInteger [] requestCells, FJavaTask [] responseCells, int myIdx, 
+      FJavaTask emptyTask) {
     for(int i = 0; i < requestCells.length; ++i) {
       if(requestCells[i].get() != EMPTY_REQUEST) 
         throw new IllegalArgumentException("All request cells should be EMPTY_REQUEST initially");
@@ -48,23 +60,51 @@ public class SenderInitiatedDeque<T> {
     this.random = new Random();    
     this.myIdx = myIdx;
     this.numWorkers = this.status.length;
-    this.tasks = new ArrayDeque<T>();
+    this.tasks = new ArrayDeque<FJavaTask>();
   } 
   
-  public T acquire() {
+  
+  public void addTask(FJavaTask task) {
+    //TODO: how to ensure that this is called on the correct thread?
+    //Idea: Can only be called by task runner
+    if(task == null) 
+        throw new IllegalArgumentException("Task cannot be null");
+    this.tasks.push(task);
+    this.updateStatus();
+  }
+  
+  public FJavaTask getTask() {
+    if(this.tasks.isEmpty()) {
+      //TODO: add counter (worker found queue empty)
+      return acquire();
+    }
+    else {
+      FJavaTask task = this.tasks.removeLast();
+      updateStatus();
+      communicate();
+      return task;
+    }
+  }
+  
+  /**
+   * Called whenever there are no tasks in this deque
+   * @return
+   */
+  private FJavaTask acquire() {
+    //TODO: measure time acquiring a task
     while(true) {
       responseCells[myIdx] = null;
       int stealIdx = this.random.nextInt(this.numWorkers);
       if(status[stealIdx].get() == VALID_STATUS 
           && requestCells[stealIdx].compareAndSet(EMPTY_REQUEST, this.myIdx)) {
+          
+          //TODO: measure time waiting?
           while(this.responseCells[this.myIdx] == emptyTask) {
-            try {
-              this.wait();
-            } catch (InterruptedException e) { }
-            this.communicate(); //TODO: why not sleep until waken up?
+            this.communicate(); //TODO: remove busy waiting
           }
+          
           if(this.responseCells[this.myIdx] != null) {
-            T newTask = this.responseCells[this.myIdx];
+            FJavaTask newTask = this.responseCells[this.myIdx];
             this.responseCells[this.myIdx] = emptyTask;
             return newTask;
           }
@@ -76,26 +116,22 @@ public class SenderInitiatedDeque<T> {
   /*
    * Respond to requests, if any.
    */
-  public void communicate() {
+  private void communicate() {
     int requestIdx = this.requestCells[this.myIdx].get();
     if(requestIdx == EMPTY_REQUEST) return;
+    
     if(this.tasks.isEmpty()) {
-      this.responseCells[this.myIdx] = null;
+      this.responseCells[requestIdx] = null;
     }
     else {
-      this.responseCells[this.myIdx] = this.tasks.removeFirst();
+      this.responseCells[requestIdx] = this.tasks.removeFirst();
     }
-    notifyAll();
     this.requestCells[this.myIdx].set(EMPTY_REQUEST);
   }
   
-  public void addTask(T task) {
-    if(task == null) 
-        throw new IllegalArgumentException("Task cannot be null");
-    this.tasks.push(task);
-    notifyAll();
+  private void updateStatus() {
+    int available = this.tasks.size() > 0 ? VALID_STATUS : INVALID_STATUS;
+    if(this.status[this.myIdx].get() != available) 
+      this.status[this.myIdx].set(available);
   }
-  
-  
-
 }
