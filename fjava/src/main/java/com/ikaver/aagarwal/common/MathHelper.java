588b5c97ab779cad74ae8a8783debe467d4b51f3
Iosef Kaver <iosef.kaver@gmail.com>
2015-04-17 20:13:21 -0400

    Fixed bug in random number generation of MathHelper

--- fjava/src/main/java/com/ikaver/aagarwal/common/MathHelper.java
+++ fjava/src/main/java/com/ikaver/aagarwal/common/MathHelper.java
@@ -3,7 +3,7 @@ package com.ikaver.aagarwal.common;
 public class MathHelper {
   
   public static long randomLongBetween(long min, long max) {
-    return min + (long)Math.floor((Math.random() * (max-min)));
+    return min + (long)(Math.floor((Math.random() * (max-min))));
   }
   
   public static double randomBetween(double min, double max) {
@@ -21,6 +21,6 @@ public class MathHelper {
    * @return a random number between [min, max]
    */
   public static int randomBetween(int min, int max) {
-	  return min + (int)Math.random() * (max - min);
+	  return min + (int)(Math.random() * (max - min));
   }
 }
