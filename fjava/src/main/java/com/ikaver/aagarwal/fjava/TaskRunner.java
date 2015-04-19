c603175b97a0b087e426017a4d2060e143ac3b34
Iosef Kaver <iosef.kaver@gmail.com>
2015-04-19 00:41:48 -0400

    Added timing code to TR

--- fjava/src/main/java/com/ikaver/aagarwal/fjava/TaskRunner.java
+++ fjava/src/main/java/com/ikaver/aagarwal/fjava/TaskRunner.java
@@ -4,6 +4,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import com.ikaver.aagarwal.common.Definitions;
+import com.ikaver.aagarwal.common.FastStopwatch;
 import com.ikaver.aagarwal.fjava.stats.StatsTracker;
 
 
@@ -16,6 +17,8 @@ public class TaskRunner implements Runnable {
   private volatile boolean shouldShutdown;
     
   private Logger log;
+  private FastStopwatch getTaskStopwatch;
+  private FastStopwatch runTaskStopwatch;
   
   /* Statistics */
   
@@ -23,7 +26,10 @@ public class TaskRunner implements Runnable {
     this.deque = deque;
     this.taskRunnerID = taskRunnerID;
     this.thread = new Thread(this, "Task Runner " + this.taskRunnerID);
-    log = LogManager.getLogger();
+    
+    this.log = LogManager.getLogger();
+    this.getTaskStopwatch = new FastStopwatch();
+    this.runTaskStopwatch = new FastStopwatch();
   }
   
   public void addTask(FJavaTask task) {
@@ -40,33 +46,51 @@ public class TaskRunner implements Runnable {
     this.shouldShutdown = shouldShutdown;
   }
   
-  
-  
   public void syncTask(FJavaTask parentTask) {
+    int triesBeforeSteal = 1;
     while(true) {
       if(parentTask.areAllChildsDone()) {
         return;
       }
       else {
+        this.getTaskStopwatch.start();
         FJavaTask task = deque.getTask(parentTask);
+        if(Definitions.TRACK_STATS)
+          StatsTracker.getInstance().onGetTaskTime(this.taskRunnerID, this.getTaskStopwatch.end());
         if(task == null) {
+          ++triesBeforeSteal;
           continue;
         }
+        if(Definitions.TRACK_STATS)
+          StatsTracker.getInstance().onTaskAcquired(this.taskRunnerID, triesBeforeSteal);
+        triesBeforeSteal = 0;
+        this.runTaskStopwatch.start();
         task.run(this);
+        if(Definitions.TRACK_STATS)
+          StatsTracker.getInstance().onRunTaskTime(this.taskRunnerID, this.runTaskStopwatch.end());
         this.notifyTaskDone(task);
       }
     }
   }
   
   public void run() {
-    while(!this.shouldShutdown) { //TODO: while not finished running all tasks, according to the pool?
-      //TODO: measure time waiting for task?
-      
+    int triesBeforeSteal = 1;
+    while(!this.shouldShutdown) {    
+      this.getTaskStopwatch.start();
       FJavaTask task = deque.getTask(null);
+      if(Definitions.TRACK_STATS)
+        StatsTracker.getInstance().onGetTaskTime(this.taskRunnerID, this.getTaskStopwatch.end());
       if(task == null) {
+        ++triesBeforeSteal;
         continue;
       }
+      if(Definitions.TRACK_STATS)
+        StatsTracker.getInstance().onTaskAcquired(this.taskRunnerID, triesBeforeSteal);
+      triesBeforeSteal = 1;
+      this.runTaskStopwatch.start();
       task.run(this);
+      if(Definitions.TRACK_STATS)
+        StatsTracker.getInstance().onRunTaskTime(this.taskRunnerID, this.runTaskStopwatch.end());
       this.notifyTaskDone(task);
     }
     log.warn("TR {} shutting down", this.taskRunnerID);
