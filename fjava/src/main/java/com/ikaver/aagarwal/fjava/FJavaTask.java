dafa18d2b15a871a3be10749a14d7ea817bac553
Iosef Kaver <iosef.kaver@gmail.com>
2015-05-02 22:44:42 -0400

    Use atomicinteger instead of keeping references to children

--- fjava/src/main/java/com/ikaver/aagarwal/fjava/FJavaTask.java
+++ fjava/src/main/java/com/ikaver/aagarwal/fjava/FJavaTask.java
@@ -1,87 +1,66 @@
 package com.ikaver.aagarwal.fjava;
 
-import java.util.ArrayList;
-
-import com.ikaver.aagarwal.common.FJavaConf;
-import com.ikaver.aagarwal.common.FastStopwatch;
-import com.ikaver.aagarwal.fjava.stats.StatsTracker;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public abstract class FJavaTask {
-  
-  private TaskRunner runner;
-  private ArrayList<FJavaTask> childTasks;
-  private boolean isDone; //TODO: is it necessary to be volatile?
-  
-  private FastStopwatch computeTime;
-  
+
+  private TaskRunner  runner;
+  private FJavaTask parent;
+  private AtomicInteger childCompleteCount;
+  private boolean     isDone;    // TODO: is it necessary to be volatile?
+
   public FJavaTask() {
-    this.childTasks = new ArrayList<FJavaTask>();
+    this.childCompleteCount = new AtomicInteger(0);
     this.isDone = false;
   }
-  
+
   public abstract void compute();
-  
+
   public void runAsync(FJavaTask parent) {
     parent.addChild(this);
     this.runner = parent.runner;
     this.runner.addTask(this);
   }
-  
+
   public void runSync(FJavaTask parent) {
     this.runner = parent.runner;
     this.compute();
     this.setIsDone(true);
   }
-    
+
   public void sync() {
     this.runner.syncTask(this);
   }
-  
-  public void startWorkTime() {
-    if(FJavaConf.shouldTrackStats()) {
-      this.computeTime = new FastStopwatch();
-      this.computeTime.start();
-    }
-  }
-  
-  public void endWorkTime() {
-    if(FJavaConf.shouldTrackStats()) {
-      StatsTracker.getInstance().onComputeTime(this.runner.getTaskRunnerID(), this.computeTime.end());
-    }
-  }
-  
+
   void execute(TaskRunner runner) {
     this.runner = runner;
     this.compute();
     this.setIsDone(true);
   }
-  
-    
+
   boolean areAllChildsDone() {
-    for(int i = 0; i < childTasks.size(); ++i) {
-      if(!childTasks.get(i).isDone()) {
-        return false;
-      }
-      else {
-        childTasks.remove(i);
-        i = -1;
-      }
-    }
-    return true;
+    return this.childCompleteCount.get() == 0;
   }
-  
+
   boolean isDone() {
     return this.isDone;
   }
-  
+
   void setIsDone(boolean done) {
-    if(this.isDone && done == false) 
+    if (this.isDone && done == false)
       throw new IllegalStateException("Cannot 'undo' a done task");
     this.isDone = done;
-    this.childTasks.clear();
+    if(this.isDone) {
+      if(this.parent != null) this.parent.onChildDone();
+    }
   }
   
+  void onChildDone() {
+    this.childCompleteCount.decrementAndGet();
+  }
+
   private void addChild(FJavaTask task) {
-    this.childTasks.add(task);
+    task.parent = this;
+    this.childCompleteCount.incrementAndGet();
   }
 }
