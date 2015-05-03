8f8dc0258811ec9197ced71f9f4a4468a9469c08
Ankit Agarwal <ankit1990@gmail.com>
2015-05-03 00:19:51 -0400

    Changing granularity for the amount of work assigned to each runner.

--- fjava/src/main/java/com/ikaver/aagarwal/common/Definitions.java
+++ fjava/src/main/java/com/ikaver/aagarwal/common/Definitions.java
@@ -3,7 +3,7 @@ package com.ikaver.aagarwal.common;
 public class Definitions {
 
   public static final int QUICKSORT_SEQ_THRESHOLD = 4000; 
-  public static final int FILTER_SEQ_THRESHOLD = 4000;
+  public static final int FILTER_SEQ_THRESHOLD = 64;
   public static final int MATRIX_MULT_SEQ_THRESHOLD = 64;
   
   public static final int BENCHMARK_ROUNDS = 5;
