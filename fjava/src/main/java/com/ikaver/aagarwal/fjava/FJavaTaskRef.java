9b4aea0f95cc71601655e087ef2f5695a5cfff0c
Iosef Kaver <iosef.kaver@gmail.com>
2015-05-03 17:59:58 -0400

    Added padded versions of FJavaTaskRef and IntRef

--- fjava/src/main/java/com/ikaver/aagarwal/fjava/FJavaTaskRef.java
+++ fjava/src/main/java/com/ikaver/aagarwal/fjava/FJavaTaskRef.java
@@ -2,7 +2,9 @@ package com.ikaver.aagarwal.fjava;
 
 public class FJavaTaskRef {
   
-  public FJavaTask task;
+  //object 16 bytes
+  public FJavaTask task; // 8 bytes
+  int a1,a2,a3,a4,a5,a6,a7,a8,a9,a10; //padding (64 bytes)
   
   public FJavaTaskRef(FJavaTask task) {
     this.task = task;
--- fjava/src/main/java/com/ikaver/aagarwal/fjava/IntRef.java
+++ fjava/src/main/java/com/ikaver/aagarwal/fjava/IntRef.java
@@ -2,7 +2,9 @@ package com.ikaver.aagarwal.fjava;
 
 public class IntRef {
   
-  public volatile int value;
+  //16 bytes (object)
+  public volatile int value; //value (4 btyes)
+  int a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11; //padding (44 bytes)
   
   public IntRef() {
     this.value = 0;
