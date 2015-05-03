5ab0c208a71663d66424a35bfbb2320e71bac239
Iosef Kaver <iosef.kaver@gmail.com>
2015-05-03 18:57:14 -0400

    Fixed padding in FJavaTaskRef

--- fjava/src/main/java/com/ikaver/aagarwal/fjava/FJavaTaskRef.java
+++ fjava/src/main/java/com/ikaver/aagarwal/fjava/FJavaTaskRef.java
@@ -3,8 +3,8 @@ package com.ikaver.aagarwal.fjava;
 public class FJavaTaskRef {
   
   //object 16 bytes
-  public FJavaTask task; // 8 bytes
-  int a1,a2,a3,a4,a5,a6,a7,a8,a9,a10; //padding (64 bytes)
+  public FJavaTask task; // 8 bytes  
+  int a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11,a12; //48 bytes
   
   public FJavaTaskRef(FJavaTask task) {
     this.task = task;
