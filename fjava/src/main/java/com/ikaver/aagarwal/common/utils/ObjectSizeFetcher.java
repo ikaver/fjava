810581a26718515ca80c8d390cb45b9e8e69e633
Ankit Agarwal <ankit1990@gmail.com>
2015-05-03 18:29:24 -0400

    Utility to determine the size of object

---
+++ fjava/src/main/java/com/ikaver/aagarwal/common/utils/ObjectSizeFetcher.java
@@ -0,0 +1,25 @@
+package com.ikaver.aagarwal.common.utils;
+
+import com.carrotsearch.sizeof.RamUsageEstimator;
+import com.ikaver.aagarwal.fjava.EmptyFJavaTask;
+import com.ikaver.aagarwal.fjava.FJavaTaskRef;
+
+
+/**
+ * Fetches the size of a java object.
+ */
+public class ObjectSizeFetcher {
+
+	/**
+	 * Returns the size i.e. the amount of real memory which it occupies.
+	 */
+	public static final long getSize(Object obj) {
+		return RamUsageEstimator.sizeOf(obj);
+	}
+
+	public static void main(String args[]) {
+		EmptyFJavaTask task = new EmptyFJavaTask();
+		FJavaTaskRef ref = new FJavaTaskRef(task);
+		System.out.println(RamUsageEstimator.sizeOf(ref));
+	}
+}
