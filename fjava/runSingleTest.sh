f699cdb3922d214189e70704682a7ba97b57949b
Iosef Kaver <iosef.kaver@gmail.com>
2015-05-05 00:18:58 -0400

    Fixing test scripts...

--- fjava/runSingleTest.sh
+++ fjava/runSingleTest.sh
@@ -1,12 +1,14 @@
 
+SLEEP_TIME=60
+
 echo 'Running FJava...'
 mvn -Dtest="$1#testFJava" test 1> out.txt 2> log.txt	
 echo 'Pause between tests...'
-sleep 60
+sleep $SLEEP_TIME
 echo 'Running Java Fork Join...'
 mvn -Dtest="$1#testJavaForkJoin" test >> out.txt		
 echo 'Pause between tests...'
-sleep 60
+sleep $SLEEP_TIME
 echo 'Running sequential...'
 mvn -Dtest="$1#testSequential" test >> out.txt	
 echo 'Processing output...'
@@ -15,10 +17,10 @@ mv /tmp/out.txt out.txt
 echo 'Creating graphs...'
 if [ "$COLLECT_STATS" = "true" ] 
 then
-	cat log.txt | grep StatsTracker | cut -d " " -f 2,3,4 | python  graphs/statsgraph.py $2 .\* > $2.txt
+	cat log.txt | grep StatsTracker | cut -d " " -f 2,3,4 | python  graphs/statsgraph.py $1 .\* > $1.txt
 else
-	cat out.txt | grep $1. -A 1 | awk 'NR % 3 != 0' |  python graphs/timegraph.py $2
+	cat out.txt | grep $1. -A 1 | awk 'NR % 3 != 0' |  python graphs/timegraph.py $1
 fi
 echo 'Sleeping for computer to chill...'
-sleep 60
+sleep $SLEEP_TIME
 echo "Done with $1"
