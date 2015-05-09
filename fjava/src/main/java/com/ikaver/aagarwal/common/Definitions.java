0bf6e39a1661fd85dfbb8933860435e597849ab4
Iosef Kaver <iosef.kaver@gmail.com>
2015-05-09 19:08:31 -0400

    Added one warmup round

--- fjava/src/main/java/com/ikaver/aagarwal/common/Definitions.java
+++ fjava/src/main/java/com/ikaver/aagarwal/common/Definitions.java
@@ -3,5 +3,5 @@ package com.ikaver.aagarwal.common;
 public class Definitions {
   
   public static final int BENCHMARK_ROUNDS = 1;
-  public static final int WARMUP_ROUNDS = 0;
+  public static final int WARMUP_ROUNDS = 1;
 }
