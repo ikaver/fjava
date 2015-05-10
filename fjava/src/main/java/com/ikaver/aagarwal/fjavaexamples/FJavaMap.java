d3abb94b42196322a8f6f7175a393b26a2916d1c
Ankit Agarwal <ankit1990@gmail.com>
2015-05-10 17:10:46 -0400

    Code to instrument Map benchmark.

--- fjava/src/main/java/com/ikaver/aagarwal/fjavaexamples/FJavaMap.java
+++ fjava/src/main/java/com/ikaver/aagarwal/fjavaexamples/FJavaMap.java
@@ -3,6 +3,7 @@ package com.ikaver.aagarwal.fjavaexamples;
 import java.util.ArrayList;
 
 import com.ikaver.aagarwal.common.FJavaConf;
+import com.ikaver.aagarwal.common.FastStopwatch;
 import com.ikaver.aagarwal.common.problems.Map;
 import com.ikaver.aagarwal.common.problems.MapFunction;
 import com.ikaver.aagarwal.fjava.FJavaPool;
@@ -16,13 +17,19 @@ public class FJavaMap<T, V> extends FJavaTask implements Map<T, V> {
   private V [] result;
   private int left;
   private int right;
-  
+  private final FastStopwatch computeTimeWatch;
+
   public static final int ITERATIONS_FOR_BALANCE = 250;
   
   private FJavaPool pool;
   
   public FJavaMap(FJavaPool pool) {
     this.pool = pool;
+    if (FJavaConf.shouldTrackStats()) {
+      computeTimeWatch = new FastStopwatch();
+    } else {
+    	computeTimeWatch = null;
+    }
   }
   
   public FJavaMap(T [] array, V [] result, 
@@ -32,6 +39,11 @@ public class FJavaMap<T, V> extends FJavaTask implements Map<T, V> {
     this.mapFunc = mapFunc;
     this.left = left;
     this.right = right;
+    if (FJavaConf.shouldTrackStats()) {
+      computeTimeWatch = new FastStopwatch();
+    } else {
+    	computeTimeWatch = null;
+    }
   }
     
   public void map(T [] array, V [] result, MapFunction<T, V> mapFunc) {
@@ -39,7 +51,10 @@ public class FJavaMap<T, V> extends FJavaTask implements Map<T, V> {
   }
   
   @Override
-  public void compute() {   
+  public void compute() {
+  	if (FJavaConf.shouldTrackStats()) {
+  		computeTimeWatch.start();
+  	}
     if(right - left <= FJavaConf.getMapSequentialThreshold()) {
       for(int i = left; i <= right; ++i) {
         this.result[i] = this.mapFunc.map(this.array[i]);
@@ -47,9 +62,15 @@ public class FJavaMap<T, V> extends FJavaTask implements Map<T, V> {
           this.tryLoadBalance();
         }
       }
+      if (FJavaConf.shouldTrackStats()) {
+        addComputeTime(computeTimeWatch.end());
+      }
       return;
     }
     else {
+    	if (FJavaConf.shouldTrackStats()) {
+    	  addComputeTime(computeTimeWatch.end());
+    	}
       ArrayList<FJavaMap<T, V>> tasks = new ArrayList<FJavaMap<T, V>>();
       while(left <= right - FJavaConf.getMapSequentialThreshold()) {
         int mid = (right+left)/2;
